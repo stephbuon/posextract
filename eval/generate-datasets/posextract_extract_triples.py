260b72cf557cfc2e09a4e9f6f9a2e23844bcd898
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-11-17 05:11:56 -0600

    Update posextract_extract_triples.py

--- eval/generate-datasets/posextract_extract_triples.py
+++ eval/generate-datasets/posextract_extract_triples.py
@@ -1,9 +1,10 @@
+sentence_type = 'comp_sent'
 d = '/home/stephbuon/Downloads/sentences-20221117T105534Z-001/sentences/hansard_sentences'
-file = open(d + '/' + 'comp_sent.txt')
+file = open(d + '/' + sentence_type + '.txt')
 lines = [line.strip('\n').replace('\"', '') for line in file.readlines()] 
 file.close()
 
-with open(d + '/' + 'test.txt', 'w+') as f:
+with open(d + '/' + 'posextract_' + sentence_type + '_triples.txt', 'w+') as f:
     for line in lines:
         f.write(line)
         f.write('\n')
